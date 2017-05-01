580f131964481ac6bee255299c32ef32556aed85
koalaah <100859189@student.swin.edu.au>
2017-05-01 17:20:18 +1000

    Testing new settings activity

--- BionicVision/app/src/main/java/software_a/com/bionicvision/LaunchActivity.java
+++ BionicVision/app/src/main/java/software_a/com/bionicvision/LaunchActivity.java
@@ -1,12 +1,54 @@
 package software_a.com.bionicvision;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
+import android.support.annotation.Nullable;
+
+//public class LaunchActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_launch);
+//    }
+//
+//    public void goToCamera(View v) {
+//        startActivity(new Intent(LaunchActivity.this, CameraActivity.class));
+//    }
+//
+//    public void goToSettings(View v) {
+//        startActivity(new Intent(LaunchActivity.this, SettingsActivity.class));
+//    }
+//
+//    public void goToAbout(View v) {
+//        startActivity(new Intent(LaunchActivity.this, AboutActivity.class));
+//    }
+//
+//    /**
+//     * A native method that is implemented by the 'native-lib' native library,
+//     * which is packaged with this application.
+//     */
+//    public native String stringFromJNI();
+//
+//    // Used to load the 'native-lib' library on application startup.
+//    static {
+//        System.loadLibrary("native-lib");
+//    }
+//}
 
 public class LaunchActivity extends AppCompatActivity {
+
+    private static final int RESULT_SETTINGS = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,28 +56,20 @@ public class LaunchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_launch);
     }
 
-    public void goToCamera(View v) {
-        startActivity(new Intent(LaunchActivity.this, CameraActivity.class));
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_scrolling, menu);
+        return true;
     }
 
-    public void goToSettings(View v) {
-        startActivity(new Intent(LaunchActivity.this, SettingsActivity.class));
-    }
-
-    public void goToAbout(View v) {
-        startActivity(new Intent(LaunchActivity.this, AboutActivity.class));
-    }
-
-
-
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
-
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Intent i = new Intent(this, SettingsActivity.class);
+                startActivityForResult(i, RESULT_SETTINGS);
+                break;
+        }
+        return true;
     }
 }
--- BionicVision/app/src/main/java/software_a/com/bionicvision/SettingsActivity.java
+++ BionicVision/app/src/main/java/software_a/com/bionicvision/SettingsActivity.java
@@ -1,29 +1,51 @@
 package software_a.com.bionicvision;
 
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class SettingsActivity extends AppCompatActivity {
+//public class SettingsActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_scrolling);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//    }
+//}
 
+public class SettingsActivity extends PreferenceActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scrolling);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new MainSettingsFragment())
+                .commit();
+    }
+
+    public static class MainSettingsFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings);
+        }
     }
 }
+
