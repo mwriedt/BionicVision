1b4760d1f12cf573d713dc359a2f50697145cc5c
Justin Adams <101091429@student.swin.edu.au>
2017-09-07 01:49:39 +1000

    added algorithm class, switch class, and settings bundle

---
+++ BionicVision/app/src/main/java/software_a/com/bionicvision/AlgorithmSwitch.java
@@ -0,0 +1,19 @@
+package software_a.com.bionicvision;
+
+/**
+ * Created by Justin on 5/09/2017.
+ */
+
+public class AlgorithmSwitch
+{
+    public AlgorithmSwitch()
+    {
+
+    }
+
+    public static Algorithm choose()
+    {
+        //switch()
+        return null;
+    }
+}
