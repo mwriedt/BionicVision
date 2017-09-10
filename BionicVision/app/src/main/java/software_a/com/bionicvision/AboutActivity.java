7a9a0045aa2a56284365d7fa716a59435b7020ea
Justin Adams <101091429@student.swin.edu.au>
2017-09-11 01:27:23 +1000

    added back button to action bar for about activity

--- BionicVision/app/src/main/java/software_a/com/bionicvision/AboutActivity.java
+++ BionicVision/app/src/main/java/software_a/com/bionicvision/AboutActivity.java
@@ -2,6 +2,7 @@ package software_a.com.bionicvision;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.WindowManager;
 
 public class AboutActivity extends AppCompatActivity {
@@ -11,6 +12,27 @@ public class AboutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // set action bar to allow backtracking
+        if (getActionBar() != null)
+        {
+            getActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
         setContentView(R.layout.activity_about);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        // determine previous activity to backtrack to
+        int act = item.getItemId();
+
+        if(act == android.R.id.home)
+        {
+            this.finish();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
