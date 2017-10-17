e4208cef937d138b56ba6f1601d66de99b14eab8
Jinsu <100052645@student.swin.edu.au>
2017-10-17 16:47:32 +1100

    permission_interface_insetting

--- BionicVision/opencv/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ BionicVision/opencv/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: /Users/koalaah/BionicVision/BionicVision/BionicVision/opencv/src/org/opencv/engine/OpenCVEngineInterface.aidl
+ * Original file: C:\\Users\\User\\Desktop\\BSC2-2\\SoftwareDev-B\\BionicVision\\BionicVision\\opencv\\src\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
